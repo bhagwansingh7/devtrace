e0abb9aebc30f453d6d2d0b67ec6952c34e643c0
user.name <bhagwansinghdhangar49@gmail.com>
2026-04-08 22:24:43 +0530

    test code

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -4,11 +4,11 @@ const { protect } = require('../../middlewares/auth.middleware');
 const { apiLimiter } = require('../../middlewares/rateLimit.middleware');
 const router = express.Router();
 
-// Public routes with rate limiting
+
 router.post('/register', apiLimiter, register);
 router.post('/login', apiLimiter, login);
 
-// Protected route
+
 router.get('/me', protect, getMe);
 
 module.exports = router;
